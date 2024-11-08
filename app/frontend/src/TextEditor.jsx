8f4230ab8607dc07092fbf787c074fecb0082f50
Fabian Ulrich <ulricfab@students.zhaw.ch>
2024-11-08 20:11:45 +0100

    text editor highlights

--- app/frontend/src/TextEditor.jsx
+++ app/frontend/src/TextEditor.jsx
@@ -1,30 +1,62 @@
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState, useRef, useCallback} from "react";
 import {useQuery} from "react-query";
 
 const fetchHighlightData = async () => {
     return [
-        {start: 10, end: 15, color: "yellow"},
-        {start: 20, end: 25, color: "lightgreen"},
+        { text: "Which deal are you actively working on?", sentiment: "positive"},
+        { text: "We have decided not to make any reps about these plans, since any buyer would not be taking them.",  sentiment: "negative"},
+        { text: "Do I need to be working on the draft agreement?",  sentiment: "neutral"},
     ];
 };
+
+const sampleEmail = "We have decided not to make any reps about these plans, since any buyer would not be taking them.  However, we have a data room list that specifies what was made available, and the other side is welcome to review most of what is in that room.  We remain cautious about releasing employment compensation data and contract information about individuals, but are happy to provide totals of current and future costs under those agreements in the aggregate.  Let me know if you have questions.\n" +
+    "\n" +
+    "Which deal are you actively working on?  Fire or Wind?  Do I need to be working on the draft agreement?\n" +
+    "\n" +
+    "Thanks.\n" +
+    "\n" +
+    "Michelle\n" +
+    "\n";
 
 export function TextEditor() {
     const {data: highlights} = useQuery("highlightData", fetchHighlightData);
     const editorRef = useRef(null);
 
+    const [textContent, setTextContent] = useState(sampleEmail);
+    const onChange = useCallback((event) => {
+        setTextContent(event.target.innerText);
+    }, []);
+
     useEffect(() => {
-        //editorRef.current.focus();
-        editorRef.current.innerHTML = "We have decided not to make any reps about these plans, since any buyer would not be taking them.  However, we have a data room list that specifies what was made available, and the other side is welcome to review most of what is in that room.  We remain cautious about releasing employment compensation data and contract information about individuals, but are happy to provide totals of current and future costs under those agreements in the aggregate.  Let me know if you have questions.\n" +
-            "\n" +
-            "Which deal are you actively working on?  Fire or Wind?  Do I need to be working on the draft agreement?\n" +
-            "\n" +
-            "Thanks.\n" +
-            "\n" +
-            "Michelle\n" +
-            "\n"
+        let newHtml = textContent;
 
+        // TODO: fetch highlights here
+        if (highlights && highlights.length > 0) {
+            const formatSentiment = (sentiment, text) => {
+                switch (sentiment) {
+                    case "positive":
+                        return `<mark style="background-color: #DFD">${text}</mark>`
+                    case "neutral":
+                        return `<mark style="background-color: #DDF">${text}</mark>`
+                    case "negative":
+                        return `<mark style="background-color: #FDD">${text}</mark>`
+                }
+            };
 
-    }, [])
+            highlights.forEach(({sentiment, text}) => {
+                newHtml = newHtml.replace(text, formatSentiment(sentiment, text));
+            })
+        }
+
+        if (newHtml !== editorRef.current.innerHTML) {
+            editorRef.current.innerHTML = newHtml;
+            editorRef.current.focus();
+        }
+    }, [textContent, highlights]);
+
+    const onPaste = (e) => {
+
+    }
 
     return <div className="flex flex-col h-full">
         <label className="text-sm text-gray-600 flex-grow-0 mb-1">E-Mail</label>
@@ -35,6 +67,7 @@ export function TextEditor() {
             style={{
                 whiteSpace: "pre-wrap", // Preserve spaces and newlines
             }}
+            onInput={onChange}
         >
         </div>
     </div>
